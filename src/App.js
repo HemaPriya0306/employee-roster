7a5a2cde8da431a501629e18bc160eed515b9357
HemaPriya0306 <hemapriyabhoopathy@gmail.com>
2025-01-17 01:54:12 +1100

    feat: add pagination for employee list table

--- src/App.js
+++ src/App.js
@@ -14,7 +14,22 @@ export const App = () => {
   });
   const [selectedEmp, setSelectedEmp] = useState(null);
   const [searchValue, setSearchValue] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);  // Current page
+  const [employeesPerPage] = useState(5);
   const modalRef = useRef();
+
+  // Calculate the total number of pages
+  const filteredEmployees = employees?.filter((emp) => emp?.firstName?.toLowerCase().includes(searchValue.toLowerCase()) ||
+    emp?.lastName?.toLowerCase().includes(searchValue?.toLowerCase()));
+
+  const totalPages = Math.ceil(filteredEmployees?.length / employeesPerPage);
+
+  // Calculate the index of the first and last employee on the current page
+  const indexOfLastEmployee = currentPage * employeesPerPage;
+  const indexOfFirstEmployee = indexOfLastEmployee - employeesPerPage;
+
+  // Slice the employee list to only show the employees for the current page
+  const currentEmployees = filteredEmployees?.slice(indexOfFirstEmployee, indexOfLastEmployee);
 
   useEffect(() => {
     dispatch(getEmployeesRequest());
@@ -26,58 +41,96 @@ export const App = () => {
     }
   }, [selectedEmp]);
 
-  if (loading) {
-    return (
-      <div className="loading-block">
-        <p>Loading...</p>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="error-block">
-        <p>Error: {error}</p>
-      </div>
-    );
+  const handleSearch = (searchValue) => {
+    setSearchValue(searchValue);
+    setCurrentPage(1);
   }
 
   const handleRowClick = (employee) => {
     setSelectedEmp(employee);
   };
 
-  return (
-    <div className="App">
-      <Header
-        companyInfo={companyInfo}
-      />
-      <main id="main-content">
-        <div className="search-bar">
-          <input
-            type="text"
-            placeholder="Search employees..."
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-            aria-label="Search employees"
+  // Handle page change
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Handle next page click
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  // Handle previous page click
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  if (loading) {
+    return (
+      <div className="loading-block">
+        <p>Loading...</p>
+      </div>
+    );
+  } else if (error) {
+    return (
+      <div className="error-block">
+        <p>Error: {error}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <Header
+          companyInfo={companyInfo}
+        />
+        <main id="main-content">
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search employees..."
+              value={searchValue}
+              onChange={(e) => handleSearch(e.target.value)}
+              aria-label="Search employees"
+            />
+          </div>
+          <EmployeesList
+            currentEmployees={currentEmployees}
+            onEmployeeClick={(employee) => handleRowClick(employee)}
+            selectedEmp={selectedEmp}
+            searchValue={searchValue}
           />
-        </div>
-        <EmployeesList
-          employees={employees}
-          onEmployeeClick={(employee) => handleRowClick(employee)}
-          selectedEmp={selectedEmp}
-          searchValue={searchValue}
-        />
-      </main>
-      {selectedEmp && (
-        <EmployeeDetailModal
-          ref={modalRef}
-          employee={selectedEmp}
-          onClose={() => setSelectedEmp(null)}
-        />
-      )}
-      <Footer />
-    </div>
-  );
+
+          {currentEmployees && <div className="pagination">
+            <button onClick={handlePrevPage} disabled={currentPage === 1}>Previous</button>
+
+            {/* Display page numbers */}
+            {[...Array(totalPages)].map((_, index) => (
+              <button
+                key={index + 1}
+                onClick={() => handlePageChange(index + 1)}
+                className={currentPage === index + 1 ? 'active' : ''}
+              >
+                {index + 1}
+              </button>
+            ))}
+
+            <button onClick={handleNextPage} disabled={currentPage === totalPages}>Next</button>
+          </div>}
+        </main>
+        {selectedEmp && (
+          <EmployeeDetailModal
+            ref={modalRef}
+            employee={selectedEmp}
+            onClose={() => setSelectedEmp(null)}
+          />
+        )}
+        <Footer />
+      </div>
+    );
+  }
+
 };
-
-
--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -2,54 +2,51 @@ import React from 'react';
 import './../assets/styles/employeesList.css';
 import avatar from './../assets/images/account_circle.png';
 
-const EmployeesList = ({ employees, onEmployeeClick, selectedEmp, searchValue }) => {
-  const filteredEmployees = employees?.filter((employee) => employee?.firstName?.toLowerCase().includes(searchValue.toLowerCase()) ||
-    employee?.lastName?.toLowerCase().includes(searchValue?.toLowerCase())
-  );
-
+const EmployeesList = ({ currentEmployees, onEmployeeClick, selectedEmp, searchValue }) => {
   return (
     <div className="emp-list-container" aria-live="polite">
-      {filteredEmployees?.length === 0 ? (
-        <p>No employees found.</p>
-      ) : (
-        <table className="emp-table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Avatar</th>
-              <th>Name</th>
-              <th>Contact Number</th>
-              <th>Address</th>
+      <table className="emp-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Avatar</th>
+            <th>Name</th>
+            <th>Contact Number</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentEmployees?.length > 0 ? (currentEmployees?.map((employee) => (
+            <tr
+              key={employee.id}
+              className={selectedEmp?.id === employee.id ? 'selected-row' : ''}
+              onClick={() => onEmployeeClick(employee)}
+              tabIndex={0}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter' || e.key === ' ') {
+                  onEmployeeClick(employee);
+                }
+              }}
+              role="button"
+              aria-label={`View details for ${employee.firstName} ${employee.lastName}`}
+            >
+              <td>{employee.id}</td>
+              <td className="emp-avatar-cell">
+                <img alt={`${employee.firstName} ${employee.lastName}'s avatar`}
+                  className="emp-avatar" src={avatar} />
+              </td>
+              <td>{employee.firstName} {employee.lastName}</td>
+              <td>{employee.contactNo}</td>
+              <td>{employee.address}</td>
             </tr>
-          </thead>
-          <tbody>
-            {filteredEmployees?.map((employee) => (
-              <tr
-                key={employee.id}
-                className={selectedEmp?.id === employee.id ? 'selected-row' : ''}
-                onClick={() => onEmployeeClick(employee)}
-                tabIndex={0}
-                onKeyDown={(e) => {
-                  if (e.key === 'Enter' || e.key === ' ') {
-                    onEmployeeClick(employee);
-                  }
-                }}
-                role="button"
-                aria-label={`View details for ${employee.firstName} ${employee.lastName}`}
-              >
-                <td>{employee.id}</td>
-                <td className="emp-avatar-cell">
-                  <img alt={`${employee.firstName} ${employee.lastName}'s avatar`}
-                    className="emp-avatar" src={avatar} />
-                </td>
-                <td>{employee.firstName} {employee.lastName}</td>
-                <td>{employee.contactNo}</td>
-                <td>{employee.address}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+          ))
+          ) : (
+            <tr>
+              <td colSpan="5" style={{textAlign: "center"}}>No employees found</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
